e01735923dce7cbba4eeea7e83054b51287b454f
Patrice VANACKER <patrice.vanacker@gmail.com>
2025-01-08 21:11:33 +0100

    GH-26 (#27)
    Add timestamp and quality to field.
    Now to change field's value, one of the Field.SetValue() methods should be called.

--- pva.SuperV.Engine/IField.cs
+++ pva.SuperV.Engine/IField.cs
@@ -22,6 +22,22 @@
         IFieldDefinition? FieldDefinition { get; set; }
 
         /// <summary>
+        /// Gets or sets the timestamp of value.
+        /// </summary>
+        /// <value>
+        /// The timestamp.
+        /// </value>
+        DateTime? Timestamp { get; }
+
+        /// <summary>
+        /// Gets or sets the quality level of the value.
+        /// </summary>
+        /// <value>
+        /// The quality <see cref="QualityLevel"/>.
+        /// </value>
+        public QualityLevel? Quality { get; }
+
+        /// <summary>
         /// Converts field's value to string. If a field formatter is defined in field definition, it's used.
         /// </summary>
         /// <returns>String representation of field's value.</returns>
---
+++ pva.SuperV.Engine/QualityLevel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pva.SuperV.Engine
+{
+    /// <summary>
+    /// Quaity levels of a <see cref="Field{T}" value./>
+    /// </summary>
+    public enum QualityLevel
+    {
+        /// <summary>
+        /// Value quality is good and can be used without any doubts.
+        /// </summary>
+        GOOD,
+        /// <summary>
+        /// Value quality is bad and should NOT be used in computations.
+        /// </summary>
+        BAD,
+        /// <summary>
+        /// Value quality is uncertain which means it's not reliable.
+        /// </summary>
+        UNCERTAIN,
+    }
+}
