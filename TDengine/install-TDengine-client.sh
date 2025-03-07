273e1c5e0c906e857268934559f774b55bf5192c
PVanack <patrice.vanacker@gmail.com>
2025-03-07 10:33:18 +0100

    GH-39 Removed sudo from install-script

--- TDengine/install-TDengine-client.sh
+++ TDengine/install-TDengine-client.sh
@@ -1,5 +1,5 @@
 tar -xf TDengine/TDengine-client-3.3.5.8-Linux-x64-Lite.tar.gz
 cd TDengine-client-3.3.5.8-lite
-sudo ./install_client.sh
-sudo echo "127.0.0.1 buildkitsandbox localhost" >> /etc/hosts
+./install_client.sh
+echo "127.0.0.1 buildkitsandbox localhost" >> /etc/hosts
 cd ..
