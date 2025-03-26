33d731b943461c2a1c8140196a85ff719583dca3
PVanack <patrice.vanacker@gmail.com>
2025-03-26 10:48:50 +0100

    Fix typo in install client

--- TDengine/install-TDengine-client.sh
+++ TDengine/install-TDengine-client.sh
@@ -1,5 +1,5 @@
 tar -xf TDengine/TDengine-client-3.3.6.0-Linux-x64-Lite.tar.gz
-cd TDengine-client-3.3.6.0-Linux-x64-Lite
+cd TDengine-client-3.3.6.0-lite
 ./install_client.sh
 echo "127.0.0.1 buildkitsandbox localhost" >> /etc/hosts
 cd ..
