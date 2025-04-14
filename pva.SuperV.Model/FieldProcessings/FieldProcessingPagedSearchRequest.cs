d2de5595d4d2aa198211c500d751950115348fcd
PVanack <patrice.vanacker@gmail.com>
2025-04-14 17:50:50 +0200

    GH-104 Remove field processings and history repository search models as they are not really useful.

--- pva.SuperV.Model/FieldProcessings/FieldProcessingPagedSearchRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace pva.SuperV.Model.FieldProcessings
-{
-    public record FieldProcessingPagedSearchRequest(int PageNumber, int PageSize, string? NameFilter, string? SortOption)
-        : PagedSearchRequest(PageNumber, PageSize, NameFilter, SortOption)
-    {
-    }
-}
--- pva.SuperV.Model/HistoryRepositories/HistoryRepositoryPagedSearchRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace pva.SuperV.Model.HistoryRepositories
-{
-    public record HistoryRepositoryPagedSearchRequest(int PageNumber, int PageSize, string? NameFilter, string? SortOption)
-        : PagedSearchRequest(PageNumber, PageSize, NameFilter, SortOption)
-    {
-    }
-}
