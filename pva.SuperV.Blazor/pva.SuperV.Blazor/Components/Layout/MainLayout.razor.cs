7b647ff460aacd68d013a4d81bb041344484eeb2
PVanack <patrice.vanacker@gmail.com>
2025-04-17 20:46:31 +0200

    GH-90 Use system dark mode.

--- pva.SuperV.Blazor/pva.SuperV.Blazor/Components/Layout/MainLayout.razor.cs
+++ pva.SuperV.Blazor/pva.SuperV.Blazor/Components/Layout/MainLayout.razor.cs
@@ -5,10 +5,19 @@ namespace pva.SuperV.Blazor.Components.Layout
 {
     public partial class MainLayout : LayoutComponentBase
     {
-        MudTheme theme = new();
-        bool isDarkMode = true;
-        bool drawerOpen = false;
+        private MudThemeProvider themeProvider = default!;
+        private MudTheme theme = new();
+        private bool isDarkMode = true;
+        private bool drawerOpen = false;
 
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                isDarkMode = await themeProvider.GetSystemPreference();
+                StateHasChanged();
+            }
+        }
         void ToggleDrawer()
         {
             drawerOpen = !drawerOpen;
